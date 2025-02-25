3ef790df7a94f71b0b5b6f0ade42d3eb6b3cd5f9
dhaniil <pemudakrinj20394@gmail.com>
2025-02-25 09:43:31 +0700

    Some ADjustment

---
+++ lang/id/menu.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'nav_group.banner' => 'Spanduk',
+    'nav_group.blog' => 'Blog',
+    'nav_group.access' => 'Mengakses',
+    'nav_group.settings' => 'Pengaturan',
+    'nav_group.activities' => 'Aktivitas'
+    ];
